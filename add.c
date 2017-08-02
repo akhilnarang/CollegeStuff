f41a66d59d77c019395131ba1f2567fc30db6b35
Akhil Narang <akhilnarang.1999@gmail.com>
2017-08-02 21:50:44 +0530

    Add add xD

---
+++ add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+    int m,n,sum;
+    printf("Enter 2 numbers:\n");
+    printf("m: ");
+    scanf("%d", &m);
+    printf("n: ");
+    scanf("%d", &n);
+    sum=m+n;
+    printf("The sum of %d and %d is %d.\n", m, n, sum);
+}
