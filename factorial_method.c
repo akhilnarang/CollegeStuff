8336c7c3bfd4b1ac025a906cb7d27e7b93786c59
Akhil's Lazy Buildbot <jenkins@akhilnarang.me>
2017-08-29 20:55:09 +0530

    Tue Aug 29 20:55:09 IST 2017 automated update of cstuff
    Signed-off-by: Akhil's Lazy Buildbot <jenkins@akhilnarang.me>

---
+++ factorial_method.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+  * Program to calculate factorial of a number calling a function recursively
+  */
+ 
+int factorial(int n) {
+    if (n==1) {
+        return 1;
+    } else {
+        return(n*factorial(n-1));
+    }
+}
+
+main() {
+    printf("\nEnter a positive integer n: ");
+    int n;
+    scanf("%d", &n);
+    if (n<1) {
+        printf("\nPlease follow instructions!\n");
+    } else {
+        printf("\nFactorial of %d is %d!\n", n, factorial(n));
+    }       
+}
---
+++ functions.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+  * Program to define and call a couple of methods to perform some mathematical
+  * operations
+  */
+  
+// Function that returns the sum of 2 integers 
+int add(int a, int b) {
+    return a+b;
+}
+
+// Function that returns the difference between 2 integers
+int subtract(int a, int b) {
+    return a-b;
+}
+
+// Function that returns the product of 2 numbers
+int multiply(int a, int b) {
+    return a*b;
+}
+
+// Function that returns the quotient of 2 numbers
+int divide(int a, int b) {
+    return a/b;
+}
+
+main() {
+    // Prompting the user for input
+    printf("\nEnter 2 integers!\n");
+    int a, b;
+    // Accepting input
+    printf("a: ");
+    scanf("%d", &a);
+    printf("b: ");
+    scanf("%d", &b);
+    // Calling the functions and calling input
+    printf("\nSum of %d and %d is %d", a, b, add(a, b));
+    printf("\nDifference between %d and %d is %d", a, b, subtract(a, b));
+    printf("\nProduct of %d and %d is %d", a, b, multiply(a, b));
+    // Checking for division by 0
+    if (!b) {
+        printf("\nCannot perform division by 0!");
+    } else {
+        printf("\nQuotient of %d and %d is %d", a, b, divide(a, b));
+    }
+    printf("\n");
+}
