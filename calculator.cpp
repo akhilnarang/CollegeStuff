46572f312c0f19d1a2838987999357c04e329e9b
Akhil's Lazy Buildbot <jenkins@akhilnarang.me>
2017-10-03 09:53:15 +0530

    Automated update of CPP from Google Drive - 10/03/2017 calculator.cpp:  * Menu driven program to perform basic mathematical operations  * Keep taking input from the user, until invalid input is entered, then exit
    complex_constructor.cpp:
     * Menu driven program to perform operations on complex numbers
     * Create a different object for each complex number
     * Also add a destructor to destroy the objects
    
    Signed-off-by: Akhil's Lazy Buildbot <jenkins@akhilnarang.me>

--- calculator.cpp
+++ calculator.cpp
@@ -16,27 +16,23 @@ class Calculator
 
     // All the functions defined below will be publicly accessible
     public:
-        void sum()
+        void sum() // Method to calculate sum of 2 numbers
         {
-            // Method to calculate sum of 2 numbers
             result=a+b;
         }
 
-        void difference()
+        void difference() // Method to calculate difference between 2 numbers
         {
-            // Method to calculate difference between 2 numbers
             result=a-b;
         }
 
-        void product()
+        void product() // Method to calculate product of 2 numbers
         {
-            // Method to calculate product of 2 numbers
             result=a*b;
         }
 
-        void quotient()
+        void quotient() // Method to calculate quotient of 2 numbers
         {
-            // Method to calculate quotient of 2 numbers
             if (b==0) { // Check for division by 0, exit if it tries to happen
                 cout<<"You can't divide by 0!\n";
                 exit(0);
@@ -44,9 +40,8 @@ class Calculator
             result=a/b;
         }
 
-        void remainder()
+        void remainder() // Method to calculate remainder of 2 numbers
         {
-            // Method to calculate remainder of 2 numbers
             if (b==0) { // Check for division by 0, exit if it tries to happen
                 cout<<"You can't divide by 0!\n";
                 exit(0);
@@ -54,9 +49,8 @@ class Calculator
             result=a%b;
         }
 
-        void input()
+        void input() // Method to prompt the user and accept 2 numbers
         {
-            // Method to prompt the user and accept 2 numbers
             cout<<"Enter 2 numbers!\n";
             cout<<"a: ";
             cin>>a;
@@ -64,16 +58,16 @@ class Calculator
             cin>>b;
         }
 
-        void display()
+        void display() // Method to display the result to the user
         {
-            // Method to display the result to the user
             cout<<"Result is "<<result<<".\n";
         }
 };
 
 int main()
 {
-Calculator c; // Creating object of class Calcualator
+// Creating object of class Calcualator
+Calculator c;
 
 char ch;
 
@@ -116,10 +110,47 @@ do {
             c.remainder();
             c.display();
             break;
-        // Default case to tell the user in the case of an invalid input
+        // Default case to exit in the case of an invalid input
         default:
             cout<<"Exiting!\n";
             exit(0);
         }
     } while (1);
 }
+/*EXECUTION
+fpllab@fpllab-OptiPlex-9020:~/Desktop/Akhil$ g++ ./calculator.cpp
+fpllab@fpllab-OptiPlex-9020:~/Desktop/Akhil$ ./a.out 
+Options:
++ for addition,
+- for subtraction,
+* for multiplication,
+/ for division,
+% for modulus, and
+anything else to exit!
+choice: +
+Enter 2 numbers!
+a: 5
+b: 6
+Result is 11.
+Options:
++ for addition,
+- for subtraction,
+* for multiplication,
+/ for division,
+% for modulus, and
+anything else to exit!
+choice: *
+Enter 2 numbers!
+a: 6
+b: 4
+Result is 24.
+Options:
++ for addition,
+- for subtraction,
+* for multiplication,
+/ for division,
+% for modulus, and
+anything else to exit!
+choice: 0
+Exiting!
+*/
--- complex_constructor.cpp
+++ complex_constructor.cpp
@@ -15,7 +15,7 @@ class Complex
     int real, imaginary;
     
     public:
-        Complex() // Default constructor
+        Complex() // Default constructor, initializes everything to 0
         {
             real=imaginary=0;
         }
@@ -24,17 +24,17 @@ class Complex
             real=r;
             imaginary=i;
         }
-        Complex add(Complex *c) // Method to add the complex numbers and return the object which contains the result
+        Complex add(Complex *c) // Method to add the complex numbers and return an object of class Complex which contains sum
         {
             return Complex(c->real+this->real, c->imaginary+this->imaginary);
         }
-        Complex subtract(Complex *c) // Method to subtract the complex numbers and return the object which contains the result
+        Complex subtract(Complex *c) // Method to subtract the complex numbers and return an object of class Complex which contains the difference
         {
             return Complex(c->real-this->real, c->imaginary-this->imaginary);
         }
-        void display() // Method to display the result
+        void display() // Method to display the the complex number stored in the calling object
         {
-            cout<<"\n\nResult:- \nReal part "<<this->real<<"\nImaginary part "<<this->imaginary<<"\n\n\n";
+            cout<<"\nReal part "<<this->real<<"\nImaginary part "<<this->imaginary<<"\n\n";
         }
         ~Complex() // Destructor for class Complex
         {
@@ -45,15 +45,15 @@ class Complex
 int main()
 {
     int real, imaginary;
-    // Declaration of variables, prompting user, and accepting input
-    cout<<"\n\nEnter values for complex number 1!\n";
+    // Declaration of variables, prompting user, and accepting input and creation of 2 objects of class Complex
+    cout<<"\nEnter values for complex number 1!\n";
     cout<<"Real: ";
     cin>>real;
     cout<<"Imaginary: ";
     cin>>imaginary;
     Complex c1(real, imaginary);
 
-    cout<<"\n\nEnter values for complex number 2!\n";
+    cout<<"\nEnter values for complex number 2!\n";
     cout<<"Real: ";
     cin>>real;
     cout<<"Imaginary: ";
@@ -61,15 +61,53 @@ int main()
     Complex c2(real, imaginary);
 
     // Prompt and take choice as input
-    cout<<"\n\nEnter 1 for addition,\n2 for subtraction(first from second)\n3 for substraction(second from first), and\nanything else to exit: ";
+    cout<<"\nEnter 1 for addition,\n2 for subtraction(first from second)\n3 for substraction(second from first), and\nanything else to exit: ";
     int c;
     cin>>c;
-    Complex c3;
-    switch (c) { // switch-case structure to check user's choice
-        case 1: c3=c1.add(&c2); break;
-        case 2: c3=c1.subtract(&c2); break;
-        case 3: c3=c2.subtract(&c1); break;
+    switch (c) { // switch-case structure to check user's choice, accordingly create an object, call the required method, and display output
+        case 1: {
+            cout<<"\nSum is: "; 
+            Complex c3=c1.add(&c2);
+            c3.display();
+            break;
+            }
+        case 2: {
+            cout<<"\nDifference is: ";
+            Complex c3=c1.subtract(&c2);
+            c3.display();
+            break;
+            }
+        case 3: {
+            cout<<"\nDifference is: ";
+            Complex c3=c2.subtract(&c1);
+            c3.display();
+            break;
+            }
         default: exit(0); // Exit on wrong input
     }
-    c3.display(); // Call display() of class Complex to show the result
 }
+/*EXECUTION
+fpllab@fpllab-OptiPlex-9020:~/Desktop/Akhil$ g++ ./complex_constructor.cpp
+fpllab@fpllab-OptiPlex-9020:~/Desktop/Akhil$ ./a.out 
+
+Enter values for complex number 1!
+Real: 8
+Imaginary: 9
+
+Enter values for complex number 2!
+Real: 6
+Imaginary: 7
+
+Enter 1 for addition,
+2 for subtraction(first from second)
+3 for substraction(second from first), and
+anything else to exit: 1
+
+Sum is: 
+Real part 14
+Imaginary part 16
+
+Destroying object 0x7ffd67b72140!
+Destroying object 0x7ffd67b72130!
+Destroying object 0x7ffd67b72120!
+*/
