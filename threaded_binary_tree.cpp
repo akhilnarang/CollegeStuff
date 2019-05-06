akhilnarang/CollegeStuff

commit c51994553b4dd096cf78ed01da2069c432422ec4

tbt: add switch-case structure
Missed this earlier as I had forgotten to copy the latest version of this

Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

threaded_binary_tree.cpp
@@ -105,6 +105,21 @@ class tree {
 
 int main() {
     tree t;
-    t.create();
-    t.inorder();
+    do {
+        cout << "1. Create" << endl;
+        cout << "2. Display inorder" << endl;
+        cout << "0. To exit" << endl;
+        cout << "ch: ";
+        cin >> ch;
+        switch (ch) {
+            case 1:
+                t.create();
+                break;
+            case 2:
+                t.inorder();
+                break;
+            default:
+                cout << "Wrong choice!" << endl;
+        }
+    } while (ch != 0);
 }

commit 01229e4d422c63f38edbda6a13241629b2452eb9

CPP: fix threaded_binary_tree
This was broken since c51994553b4dd096cf78ed01da2069c432422ec4

Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

threaded_binary_tree.cpp
@@ -105,6 +105,7 @@ class tree {
 
 int main() {
     tree t;
+    int ch;
     do {
         cout << "1. Create" << endl;
         cout << "2. Display inorder" << endl;