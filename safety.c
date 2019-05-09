380dd45576446fdcfe048a583794a148316a8677
Akhil Narang <akhilnarang.1999@gmail.com>
2019-05-09 07:07:00 +0530

    safety: make it work properly
    If we are taking total as input, current code didn't work, it expected only available
    So just calculate available
    
    Also remove snapshot
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- safety.c
+++ safety.c
@@ -3,7 +3,7 @@
 #define MAX 10
 
 int i, j, processes, resourceTypes, need[MAX][MAX], allocate[MAX][MAX],
-    max[MAX][MAX], available[MAX], resourceInstances[MAX];
+    max[MAX][MAX], available[MAX], total[MAX], resourceInstances[MAX];
 
 void matrixInput(int matrix[MAX][MAX]) {
     for (i = 0; i < processes; i++) {
@@ -24,11 +24,11 @@ void input() {
     printf("resourceTypes: ");
     scanf("%d", &resourceTypes);
 
-    printf("Enter available resources!\n");
+    printf("Enter total resources!\n");
     for (i = 0; i < resourceTypes; i++) {
-        printf("Enter available resources for %d!\n", i + 1);
-        printf("available[%d]: ", i);
-        scanf("%d", &available[i]);
+        printf("Enter total resources for %d!\n", i + 1);
+        printf("total[%d]: ", i);
+        scanf("%d", &total[i]);
     }
 
     printf("Enter allocated resources for each process!\n");
@@ -42,67 +42,51 @@ void input() {
             need[i][j] = max[i][j] - allocate[i][j];
         }
     }
+
+    for (i = 0; i < resourceTypes; i++) {
+        int temp = 0;
+        for (j = 0; j < processes; j++) {
+            temp += allocate[j][i];
+        }
+        available[i] = total[i] - temp;
+    }
 }
 
 void safety() {
     int work[resourceTypes];
     int finish[processes];
-    int isSafe = 0, count = 0, cnt = 0;
+    int safeSequence[processes];
+    int isSafe = 0, count = 0;
+    int i, j, found;
 
-    for (int i = 0; i < processes; i++) finish[i] = 0;
+    for (i = 0; i < processes; i++) finish[i] = 0;
 
-    for (int i = 0; i < resourceTypes; i++) work[i] = available[i];
+    for (i = 0; i < resourceTypes; i++) work[i] = available[i];
 
-    printf("\n\nThe Probable Safety Sequence is :- \n\n");
-    printf("\t<<\t ");
-
-    int i, j;
-    while (count < (resourceTypes * processes * processes)) {
+    while (count < processes) {
+        found = 0;
         for (i = 0; i < processes; i++) {
-            if (finish[i] == 0) {
-                for (j = 0; j < resourceTypes; j++) {
+            if (!finish[i]) {
+                for (j = 0; j < resourceTypes; j++)
                     if (need[i][j] > work[j]) break;
-                }
+
                 if (j == resourceTypes) {
                     for (int k = 0; k < resourceTypes; k++)
                         work[k] += allocate[i][k];
                     finish[i] = 1;
-                    cnt++;
-                    printf("P%d\t", i);
+                    found = 1;
+                    safeSequence[count++] = i;
                 }
             }
         }
-        count++;
     }
-    if (cnt == processes)
-        printf(
-            ">>\n Thus SAFE SEQUENCE achieved!  The system is in SAFE "
-            "STATE!/n/n");
-    else
-        printf(
-            "...........\n Thus SAFE SEQUENCE not achieved!  The system is in "
-            "UNSAFE STATE!\n\n");
-}
-
-void snapshot() {
-    printf("\tProcess\tAllocated\tMaximum\tNeed\tAvailable\n");
-    for (int i = 0; i < processes; i++) {
-        printf("P%d\t\t", i);
-        for (int j = 0; j < resourceTypes; j++) {
-            printf("%d\t\t", allocate[i][j]);
-        }
-        printf("\t\t");
-        for (int j = 0; j < resourceTypes; j++) {
-            printf("%d ", max[i][j]);
-        }
-        printf("\t\t");
-        for (int j = 0; j < resourceTypes; j++) {
-            printf("%d ", need[i][j]);
-        }
-        printf("\t\t");
-        if (i == 0) {
-            for (int j = 0; j < resourceTypes; j++) printf("%d ", available[j]);
-        }
+    if (!found) {
+        printf("The system is in unsafe state!\n");
+        return;
+    }
+    printf("The system is in safe state! Safety sequence is: ");
+    for (i = 0; i < processes; i++) {
+        printf("%d\t", safeSequence[i]);
     }
 }
 
