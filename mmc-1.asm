a1766728240b939ad643cf52ae6e221d57230f81
Akhil Narang <akhilnarang.1999@gmail.com>
2019-05-12 10:39:40 +0530

    asm: turns out this *was* assignment 1
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- mmc-1.asm
+++ mmc-1.asm
@@ -1,54 +1,102 @@
-%macro rw 4
-mov rax, %1
-mov rdi, %2
-mov rsi, %3
-mov rdx, %4
+; Macro to display values
+%macro print 2
+mov rax, 1
+mov rdi, 1
+mov rsi, %1
+mov rdx, %2
 syscall
 %endmacro
-%macro print 2
-rw 1, 1, %1, %2
-%endmacro
-%macro read 2
-rw 0, 0, %1, %2
-%endmacro
-section .data ; section for initialized data
-    msg db "Sum is: "
-    msgLength equ $-msg
-    exc db "!", 10
-    excLength equ $-exc
 
-section .bss ; section for uninitialized data
-    num resb 3
-    arr resb 5
-    tmp1 resb 4
-    tmp2 resb 4
-    tmp3 resb 4
+; Section for uninitialized data
+section .bss
+count resb 1
+displayBuffer resb 4
 
-section .text ; section for text
+; Section for initialized data
+section .data
+array db 24h, 27h, 0Bh, 0Fh, 17h
+msg db "Sorted array is: "
+msgLength equ $-msg
+newLine db 10
+comma db ", "
+
+section .text
 global _start
 _start:
-    print msg, msgLength
-    mov al, 05
-    mov bl, 04
-    add al, bl
-    mov bp, 2  
-    display:
-    rol al, 4
-    mov [tmp1], al
-    and al, 0FH
-    cmp al, 09H ; Compare the byte with 0x09
-    jbe down1 ; Jump down if less or equal
-    add al, 07H ; Add 0x07 
-    down1:
-    add al, 30H ; Add 0x30 
-    mov [tmp2],	al
-    print tmp2, 1
-    mov al, [tmp1]
-    dec bp
-    jnz display
-    
-    print exc, excLength
-    ; Exit syscall
-    mov rax,60
-    mov rdi,0
-    syscall
+
+; 5 counts (as outer loop)
+mov bl, 05h
+
+l1:
+; Create 2 pointers to array, increment 1 so as to check consecutive elements
+mov rsi, array
+mov rdi, array
+inc rdi
+; 4 counts (as inner loop)
+mov cl, 04h
+
+l2:
+; Compare consecutive elements, swap if not in order
+mov al, [rsi]
+cmp al, [rdi]
+jbe l3
+xchg al, [rdi]
+mov [rsi], al
+
+l3:
+; After one iteration, increment both pointers, decrement counters
+inc rsi
+inc rdi
+dec cl
+jnz l2
+dec bl
+jnz l1
+; Jump only if not zero, i.e. keep running the loop until counter reaches 0
+
+; Display message
+print msg, msgLength
+; Pointer to array in rsi
+mov rsi, array
+; Set count to 5
+mov byte[count], 5
+display:
+; Copy to register bh, push to stack, call procedure, pop from stack
+mov bh,[rsi]
+push rsi
+call proc
+; Print result given from procedure
+print displayBuffer, 2
+print comma, 2
+pop rsi
+; Increment pointer, decrement counter, loop again if not 0
+inc rsi
+dec byte[count]
+jnz display
+print newLine, 1
+
+; Exit syscall
+mov rax, 60
+mov rdi, 0
+syscall
+
+; Procedure to get the value for us to display
+proc:
+mov rsi, displayBuffer
+mov rcx,4 
+label:
+rol bx,4
+mov al, bl
+and al, 0Fh
+cmp al, 09h
+jbe down
+add al, 07h
+            
+down:
+add al, 30h
+mov [rsi], al
+inc rsi
+dec rcx
+jnz label       
+ret
+       
+
