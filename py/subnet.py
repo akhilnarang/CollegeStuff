9ec5d87ac1ca464d37b464fee876ad86f1ed1e94
Akhil Narang <akhilnarang.1999@gmail.com>
2019-09-24 11:46:08 +0530

    Update subnet
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- py/subnet.py
+++ py/subnet.py
@@ -1,26 +1,23 @@
 #!/usr/bin/env python3
+import re
 
 
-def get_value(mask):
-    ret = ""
-    for i in mask:
-        for j in range(8 - len(i)):
-            ret += "0"
-        ret += i
-    return ret
+def convertToDecimal(binary):
+    decimal = []
+    for i in range(0, len(binary), 8):
+        decimal.append(str(int(binary[i : i + 8], 2)))
+    return ".".join(decimal)
 
 
 ip_str = input("Enter IP: ").split(".")
 ip = [int(x) for x in ip_str]
 
 default_masks = {
-    "A": "255.0.0.0",
-    "B": "255.255.0.0",
-    "C": "255.255.255.0",
-    "D": "255.255.255.255",
+    "A": ["255.0.0.0", 24],
+    "B": ["255.255.0.0", 16],
+    "C": ["255.255.255.0", 8],
 }
 
-bits = {"A": 8, "B": 16, "C": 24, "D": 32}
 
 if ip[0] < 1:
     print("Invalid IP")
@@ -28,12 +25,17 @@ if ip[0] < 1:
 
 if ip[0] <= 126:
     ip_class = "A"
-elif ip[0] >= 128 and ip[0] < 192:
+    taken_for_subnet = ".".join(map(str, ip[:1])) + "."
+elif 128 <= ip[0] < 192:
     ip_class = "B"
-elif ip[0] >= 192 and ip[0] < 224:
+    taken_for_subnet = ".".join(map(str, ip[:2])) + "."
+elif 192 <= ip[0] < 224:
     ip_class = "C"
-elif ip[0] >= 224 and ip[0] < 240:
+    taken_for_subnet = ".".join(map(str, ip[:3])) + "."
+elif 224 <= ip[0] < 240:
     ip_class = "D"
+    print("Class E IP, nothing to do!")
+    exit(1)
 else:
     ip_class = "E"
     print("Class E IP, nothing to do!")
@@ -45,20 +47,30 @@ print("Default mask is {}".format(default_masks[ip_class]))
 ip_binary = [bin(x)[2:] for x in ip]
 print("Binary representation is {}".format(".".join(ip_binary)))
 
-number_hosts = int(input("Enter number of hosts: "))
-i = 1
-while True:
-    result = 2 ** i
-    if number_hosts <= result:
-        break
-    i += 1
+ip_range = int(input("Enter number of hosts: ")) + 2
 
-bits_needed = i + bits[ip_class]
-remaining_bits = 32 - bits_needed
-binary_mask = [bin(int(x))[2:] for x in default_masks[ip_class].split(".")]
-print(binary_mask)
-zeroed = get_value(binary_mask)
-print(bits_needed)
-print(remaining_bits)
-binary_value = "1" * bits_needed + "0" * remaining_bits
-print(binary_value)
+# To get the closest power of 2
+pow = 1
+while True:
+    result = 2 ** pow
+    if ip_range <= result:
+        break
+    pow += 1
+
+mask = ("0" * pow).rjust(default_masks[ip_class][1], "1")
+n = 2 ** (default_masks[ip_class][1] - pow)
+subnets = []
+for k in range(n):
+    start = re.sub("1+", str(bin(k)[2:]), mask).rjust(default_masks[ip_class][1], "0")
+    end = start[:-pow] + "1" * pow
+    subnets.append([start, end])
+
+print("\nThe subnet mask is", default_masks[ip_class][0] + convertToDecimal(mask), "\n")
+
+print("subnets :")
+for i in subnets:
+    print(
+        taken_for_subnet + convertToDecimal(i[0]),
+        "-",
+        taken_for_subnet + convertToDecimal(i[1]),
+    )
