cef544310209e8a515f87a87cafcb6d452f3a9e3
Akhil Narang <akhilnarang.1999@gmail.com>
2020-01-06 11:49:38 +0530

    CollegeStuff: add apriori
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

---
+++ py/apriori.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+
+def print_data(data: dict):
+    print("Items\tQuantity")
+    for k, v in data.items():
+        print(*k, end="")
+        print(f"\t {v}")
+
+
+def _print(data, newline=True):
+    if newline:
+        print()
+    print(data)
+    if newline:
+        print()
+
+
+def increment(data: dict, key):
+    key = frozenset(key)
+    if key in data.keys():
+        data[key] += 1
+    else:
+        data[key] = 1
+    return data
+
+
+dataset = (
+    (1, 2, 5),
+    (2, 4),
+    (2, 3),
+    (1, 2, 4),
+    (1, 3),
+    (2, 3),
+    (1, 3),
+    (1, 2, 3, 5),
+    (1, 2, 3),
+)
+minimum_support = 2
+
+_print(f"Minimum support value is {minimum_support}")
+
+_print("1 item")
+
+cluster1 = {}
+for data in dataset:
+    for d in data:
+        increment(cluster1, [d])
+print_data(cluster1)
+
+_print("After removing values less than minimum support:")
+
+for key in list(cluster1.keys()):
+    if cluster1[key] < minimum_support:
+        cluster1.pop(key)
+print_data(cluster1)
+
+_print("2 items")
+
+cluster2 = {}
+keys = list(cluster1.keys())
+for i in range(len(keys)):
+    for j in range(i + 1, len(keys)):
+        cluster2[keys[i].union(keys[j])] = 0
+
+for data in dataset:
+    for key in cluster2.keys():
+        if key.issubset(data):
+            cluster2[key] += 1
+
+print_data(cluster2)
+
+_print("After removing values less than minimum support:")
+
+for key in list(cluster2.keys()):
+    if cluster2[key] < minimum_support:
+        cluster2.pop(key)
+print_data(cluster2)
+
+_print("3 items")
+
+cluster3 = {}
+for data in dataset:
+    for i in range(len(data)):
+        for j in range(i + 1, len(data)):
+            for k in range(j + 1, len(data)):
+                increment(cluster3, [data[i], data[j], data[k]])
+print_data(cluster3)
+
+_print("After removing values less than minimum support:")
+
+keys = list(cluster3.keys())
+for key in keys:
+    if cluster3[key] < minimum_support:
+        cluster3.pop(key)
+print_data(cluster3)
