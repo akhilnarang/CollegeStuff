d0c517706fe07f8e062137dda7392e6496c06ed4
Akhil Narang <akhilnarang.1999@gmail.com>
2020-01-28 00:15:06 +0530

    matrix multiplication: only square matrices supported now, didn't wanna fix others
    Some changes + partially untested OpenMP stuff
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- cstuff/matrixmultiply.c
+++ cstuff/matrixmultiply.c
@@ -1,73 +1,94 @@
+#include <omp.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * Program to take in elements of a matrix in a DDA and display the product
- * DOES NOT WORK :D
  */
 
-void main() {
-    int ra, ca, rb, cb;
-    printf("Enter the number of rows of a: ");
-    scanf("%d", &ra);
-    printf("Enter the number of columns of a: ");
-    scanf("%d", &ca);
-    int a[ra][ca];
-    printf("Enter the number of rows of b: ");
-    scanf("%d", &rb);
-    printf("Enter the number of columns of b: ");
-    scanf("%d", &cb);
-    int b[rb][cb];
-    int p[ra][cb];
-    if (ca != rb) {
-        printf("\nColumns of a should be equal to rows of b!");
-    } else {
-        int i, j, k;
-
-        printf("\nEnter elements of a:\n");
-        for (i = 0; i < ra; i++) {
-            for (j = 0; j < ca; j++) {
-                printf("a[%d][%d]: ", i + 1, j + 1);
-                scanf("%d", &a[i][j]);
-            }
-        }
-
-        printf("\nEnter elements of b:\n");
-        for (i = 0; i < rb; i++) {
-            for (j = 0; j < cb; j++) {
-                printf("b[%d][%d]: ", i + 1, j + 1);
-                scanf("%d", &b[i][j]);
-            }
-        }
-
-        printf("\nElements of a are:\n");
-
-        for (i = 0; i < ra; i++) {
-            for (j = 0; j < ca; j++) {
-                printf("a[%d][%d] - %d\t", i + 1, j + 1, a[i][j]);
-            }
-            printf("\n");
-        }
-
-        printf("\nElements of b are:\n");
-        for (i = 0; i < rb; i++) {
-            for (j = 0; j < cb; j++) {
-                printf("b[%d][%d] - %d\t", i + 1, j + 1, b[i][j]);
-            }
-            printf("\n");
-        }
-
-        printf("\nProduct is:\n");
-        int t;
-        for (i = 0; i < ra; i++) {
-            for (j = 0; j < cb; j++) {
-                for (k = 0; k < rb; k++) {
-                    t += a[k][j] * b[j][k];
-                }
-                printf("product[%d][%d] - %d\t", i + 1, j + 1, t);
-                t = 0;
-            }
-            printf("\n");
+int MAX = RAND_MAX;
+void reset_matrix(int a[][MAX], int n) {
+    int i, j;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            a[i][j] = 0;
         }
     }
-    printf("\n");
+}
+
+void accept_matrix(int a[][MAX], int n, char* name) {
+    int i, j;
+    printf("\nEnter elements of %s:\n", name);
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%s[%d][%d]: ", name, i + 1, j + 1);
+            scanf("%d", &a[i][j]);
+        }
+    }
+}
+
+void fill_matrix(int a[][MAX], int n, char* name) {
+    int i, j;
+    printf("\nFilling elements of %s:\n", name);
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            a[i][j] = rand();
+        }
+    }
+}
+
+void display_matrix(int a[][MAX], int n, char* name) {
+    int i, j;
+    printf("\nElements of %s are:\n", name);
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%s[%d][%d] - %d\t", name, i + 1, j + 1, a[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int i, j, k, n;
+    double s;
+    int a[MAX][MAX];
+    int b[MAX][MAX];
+    int product[MAX][MAX];
+    printf("Enter the number of rows/columns (<= %d)\n", MAX);
+    printf("n: ");
+    scanf("%d", &n);
+    if (n > MAX) {
+        printf("Please enter proper values next time!\n");
+        exit(1);
+    }
+    fill_matrix(a, n, "a");
+    fill_matrix(b, n, "b");
+    display_matrix(a, n, "a");
+    display_matrix(b, n, "b");
+    reset_matrix(product, n);
+    s = omp_get_wtime();
+#pragma omp parallel for private(i, j, k) shared(a, b, product)
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            for (k = 0; k < n; k++) {
+                product[i][j] += a[k][j] * b[j][k];
+            }
+        }
+    }
+    s = omp_get_wtime() - s;
+    printf("Parallel execution time = %f\n", s);
+
+    reset_matrix(product, n);
+    s = omp_get_wtime();
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            for (k = 0; k < n; k++) {
+                product[i][j] += a[k][j] * b[j][k];
+            }
+        }
+    }
+    s = omp_get_wtime() - s;
+    printf("Serial execution time = %f\n", s);
+
+    display_matrix(product, n, "product");
 }
