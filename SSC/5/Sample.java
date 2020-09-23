82f885aa3a756f758a24cdd8ad4a037ffd09b2aa
Akhil Narang <me@akhilnarang.dev>
2020-09-23 10:24:41 +0530

    Add in SSC 5th
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

---
+++ SSC/5/Sample.java
@@ -0,0 +1,22 @@
+class Sample
+{
+  public static void main(String args[])
+  {
+    int n=Math.abs(Integer.valueOf(args[0]));
+    int rev=0,t=n;
+    while(n>0)
+    {
+      int rem=n%10;
+      rev=rev*10+rem;
+      n/=10;
+    }
+    if (rev==t)
+    {
+      System.out.println(t+" is a palindrome");
+    }
+    else
+    {
+      System.out.println(t+" is not a palindrome");
+    }
+  }
+}
