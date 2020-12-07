7cde9126645732fcd46690eed364370c541110d1
Akhil Narang <me@akhilnarang.dev>
2020-12-07 10:38:07 +0530

    dfcl3: some miscellaneous changes (adjust output format a bit)
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- dfcl/3/router_logs.py
+++ dfcl/3/router_logs.py
@@ -2,17 +2,25 @@
 
 import csv
 
+log_file_name = 'log.csv'
+print(f'Reading log file {log_file_name}!')
+
 # Read the log file
 with open('log.csv', 'r') as log:
 
-    # Parse as CSV
-    router_log = list(csv.reader(log))[1:]
+    print(f'Parsing {log_file_name} as CSV!')
 
+    # Parse as CSV
+    router_log = list(csv.reader(log))
+    print()
+    print('Sr. No.\tSource IP\tDestination IP\tProtocol')
+    srno = 0
     # Iterate over the rows
-    for row in router_log:
+    for row in router_log[1:]:
+        srno += 1
 
         # Extract the data
         src, dest, protocol = row
 
         # Print the data
-        print(f'{src} connected to {dest} over {protocol}')
+        print(f'{srno}\t{src}\t{dest}\t{protocol}')
